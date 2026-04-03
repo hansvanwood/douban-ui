c0e1697f9b7b214933b718e43e34d991cf61214d
hanswood <hanswood@icloud.com>
2026-04-03 10:37:38 +0800

    feat：跟随后端修改电影评论接口地址。

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -5,4 +5,4 @@ import request from './request'
 
 /** 获取电影评论列表 */
 export const fetchComments = (movieId: number | string, pageNum: number = 1, pageSize: number = 20) =>
-    request.get(`/comments/${movieId}`, { params: { pageNum, pageSize } })
+    request.get(`/movies/${movieId}/comments`, { params: { pageNum, pageSize } })
